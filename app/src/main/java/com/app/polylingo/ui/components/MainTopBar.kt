72a49163ec2c6dfe8178162e4e45344804f5a379
Igor <igorsiergiej@gmail.com>
2022-11-29 13:31:00 +0000

    Got a DropDownMenu working with a text field

--- app/src/main/java/com/app/polylingo/ui/components/MainTopBar.kt
+++ app/src/main/java/com/app/polylingo/ui/components/MainTopBar.kt
@@ -4,7 +4,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
-import androidx.compose.material3.BottomAppBarDefaults.FloatingActionButtonElevation.elevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/app/polylingo/ui/language/LanguageScreen.kt
+++ app/src/main/java/com/app/polylingo/ui/language/LanguageScreen.kt
@@ -1,21 +1,113 @@
 package com.app.polylingo.ui.language
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.ui.geometry.Size
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+import androidx.compose.ui.window.PopupProperties
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import com.app.polylingo.R
 import com.app.polylingo.ui.components.LanguageScaffold
 import com.app.polylingo.ui.navigation.Screen
+
+@Composable
+fun MyUI() {
+    var selectedItem by remember { mutableStateOf("") }
+    val items = listOf(
+        "Italian",
+        "German",
+        "Portuguese",
+        "Portuguese",
+        "Portuguese",
+        "Portuguese",
+        "Portuguese",
+        "Portuguese",
+        "Portuguese",
+        "Portuguese"
+    )
+    MyDropDownMenu(
+        selectedItem = selectedItem,
+        changeText = { selectedItem = it },
+        items = items
+    )
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun MyDropDownMenu(
+    selectedItem :String,
+    changeText : (String) -> Unit = {},
+    items: List<String>
+) {
+
+    var expanded by remember { mutableStateOf(false) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    var mTextFieldSize by remember { mutableStateOf(Size.Zero) }
+    var offset = DpOffset.Zero
+
+    // THIS WORKS
+    // CHANGE THIS SLOWLY TO THE EDITABLE BOX
+
+    OutlinedTextField(
+        value = selectedItem,
+        onValueChange = {
+            changeText(it)
+            expanded = !expanded
+        },
+        modifier = Modifier.onGloballyPositioned { coordinates ->
+            mTextFieldSize = coordinates.size.toSize()
+        }
+    )
+    val filteringOptions =
+        items.filter { it.contains(selectedItem, ignoreCase = true) }
+    DropdownMenu(
+        offset = offset,
+        properties = PopupProperties(focusable = false),
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+        modifier = Modifier
+            .width(with(LocalDensity.current) { mTextFieldSize.width.toDp() })
+            .background(
+                Color.Red
+            )
+            .requiredSizeIn(maxHeight = 200.dp)
+    ) {
+        if (filteringOptions.isEmpty()) {
+            expanded = false
+        } else {
+            if (filteringOptions.size == 1){
+                offset = DpOffset(x = 0.dp,y = (300).dp)
+            }
+            filteringOptions.forEachIndexed { index, s ->
+                DropdownMenuItem(onClick = {
+                    changeText(s)
+                    expanded = false
+                    keyboardController?.hide()
+                },
+                    text = { Text(text = s) })
+            }
+        }
+    }
+}
+
 
 @Composable
 fun LanguageScreen(navController: NavController) {
@@ -34,6 +126,7 @@ fun LanguageScreen(navController: NavController) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun LanguageScreenContent(
     modifier: Modifier = Modifier,
@@ -52,7 +145,7 @@ private fun LanguageScreenContent(
 
         Spacer(modifier = Modifier.height(20.dp))
 
-
+        //CHANGE THIS TO OUTLINED WHICH HAS SUPPORTING TEXT
         TextField(
             value = currentLanguage.value,
             onValueChange = { newText ->
@@ -112,5 +205,7 @@ private fun LanguageScreenContent(
                 modifier = Modifier.padding(top = 15.dp, end = 15.dp)
             )
         }
+
+        MyUI()
     }
 }
