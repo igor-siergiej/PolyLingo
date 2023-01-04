ba2f566274145022541ef011c911087665a31a70
Igor Siergiej <igorsiergiej@gmail.com>
2023-01-04 23:29:36 +0100

    Made letters appear in selectionGrid

--- app/src/main/java/com/app/polylingo/ui/games/WordOrderScreen.kt
+++ app/src/main/java/com/app/polylingo/ui/games/WordOrderScreen.kt
@@ -1,19 +1,35 @@
 package com.app.polylingo.ui.games
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.app.polylingo.R
+import com.app.polylingo.model.Entry
 import com.app.polylingo.model.EntryViewModel
 import com.app.polylingo.ui.components.scaffolds.GameScaffold
 import com.app.polylingo.ui.navigation.Screen
+import java.util.*
 
 @Composable
 fun WordOrderScreen(
@@ -62,23 +78,11 @@ private fun WordOrderScreenContent(
     val entries =
         remember { entryList!!.asSequence().shuffled().take(numOfWords).toList().distinct() }
 
-    val isFound = remember { mutableListOf<Boolean>() }
-    if (isFound.isEmpty()) {
-        for (i in 0 until numOfWords * 2) {
-            isFound.add(false)
-        }
-    }
 
-    val words = remember { mutableListOf<String>() }
 
-    if (words.isEmpty()) {
-        entries.forEach { entry ->
-            words.add(entry.translatedWord)
-        }
-        val temp = words.shuffled()
-        words.clear()
-        words.addAll(temp)
-    }
+    val word = remember { entries.first().word.toList().shuffled().joinToString(separator = "") }
+
+    var currentlySelected by remember { mutableStateOf("_".repeat(word.length)) }
 
     var openOutOfTimeDialog by remember { mutableStateOf(false) }
     var openCompletedDialog by remember { mutableStateOf(false) }
@@ -89,8 +93,46 @@ private fun WordOrderScreenContent(
             .fillMaxSize()
             .padding(5.dp)
     ) {
+        CreateLetterGrid(
+            word,
+            currentlySelected,
+            setOpenDialog = {
+                openCompletedDialog = true
+            },
+            updateSelection = {
+                var string = currentlySelected.toCharArray()
+                string[it.second] = it.first
+                currentlySelected = String(string)
+            }
+        )
 
-        //TODO create word order grid?
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            FilledTonalIconButton(onClick = {
+                //reset selection
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Refresh,
+                    contentDescription = stringResource(id = R.string.time_left_description)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        CreateSelectionGrid(
+            word,
+            currentlySelected,
+            setOpenDialog = {
+                openCompletedDialog = true
+            }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
 
         CreateTimer(
             time = time,
@@ -119,6 +161,111 @@ private fun WordOrderScreenContent(
         }
     }
 }
+
+@Composable
+fun CreateLetterGrid(
+    entry: String,
+    currentlySelected: String,
+    updateSelection: (Pair<Char,Int>) -> Unit = {},
+    setOpenDialog: () -> Unit = {}
+) {
+    val numOfColumns = 4
+    val cellBackgroundColor = MaterialTheme.colorScheme.surfaceColorAtElevation(5.dp)
+
+    val wordArray = entry.toCharArray()
+
+    val colors = remember { mutableStateMapOf<Int, Color>() }
+
+    wordArray.forEachIndexed { index, _ ->
+        if (colors[index] == null) {
+            colors[index] = cellBackgroundColor
+        }
+    }
+
+    LazyVerticalGrid(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        columns = GridCells.Fixed(numOfColumns)
+    ) {
+        items(wordArray.size) { index ->
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = colors[index]!!
+                ),
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline),
+                shape = RoundedCornerShape(size = 10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        updateSelection(wordArray[index] to index)
+                        colors[index] = Color.Green
+                    },
+                content = {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = wordArray[index].toString().uppercase(Locale.ROOT),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .padding(vertical = 15.dp)
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun CreateSelectionGrid(
+    word: String,
+    currentlySelected: String,
+    setOpenDialog: () -> Unit = {}
+) {
+    val numOfColumns = 4
+
+    val cellBackgroundColor = MaterialTheme.colorScheme.surfaceColorAtElevation(5.dp)
+
+    val colors = remember { mutableStateMapOf<Int, Color>() }
+
+    currentlySelected.forEachIndexed { index, _ ->
+        if (colors[index] == null) {
+            colors[index] = cellBackgroundColor
+        }
+    }
+    println(currentlySelected)
+
+    LazyVerticalGrid(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        columns = GridCells.Fixed(numOfColumns)
+    ) {
+        items(currentlySelected.length) { index ->
+            Box(
+                modifier = Modifier
+                    .border(BorderStroke(1.dp, MaterialTheme.colorScheme.outline))
+                    .background(colors[index]!!)
+                    .fillMaxSize()
+            ) {
+                Text(
+                    text = currentlySelected[index].uppercase(Locale.ROOT),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(vertical = 10.dp)
+                )
+            }
+        }
+    }
+}
+
+
 
 
 
